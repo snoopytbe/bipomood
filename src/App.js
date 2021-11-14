26937e62e3d46882d9e91eb0eb7d49fb0dd7e9e2
Matthieu ANDRE <matthieu.andre.77@gmail.com>
2021-11-14 13:46:49 +0100

    Diverses corrections + sauvegarde automatique

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Rating, Stack, Typography } from "@mui/material";
+import { Rating, Stack, Typography, Button, TextField } from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
-import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
 import DateAdapter from "@mui/lab/AdapterMoment";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import { MobileDatePicker } from "@mui/lab";
+import { MobileDatePicker, LocalizationProvider } from "@mui/lab";
 import awsconfig from "./aws-exports";
 import Amplify, { API, graphqlOperation } from "aws-amplify";
 import { listMoodss } from "./graphql/queries";
@@ -13,7 +10,6 @@ import { createMoods, updateMoods } from "./graphql/mutations";
 import moment from "moment";
 import MomentUtils from "@date-io/moment";
 import "moment/locale/fr";
-import { visuallyHidden } from "@mui/utils";
 
 Amplify.configure(awsconfig);
 
@@ -26,16 +22,17 @@ const App = () => {
   );
 
   async function saveData(event) {
+    //console.log(JSON.stringify(event));
     try {
       if (event.id) {
         await API.graphql(
           graphqlOperation(updateMoods, {
             input: {
               date: event.date,
-              deprime: event.deprime,
-              angoisse: event.angoisse,
-              enervement: event.enervement,
-              fatigue: event.fatigue,
+              deprime: event.deprime + "",
+              angoisse: event.angoisse + "",
+              enervement: event.enervement + "",
+              fatigue: event.fatigue + "",
               comment: event.comment || "",
               id: event.id,
             },
@@ -45,11 +42,11 @@ const App = () => {
         await API.graphql(
           graphqlOperation(createMoods, {
             input: {
-              date: event.date,
-              deprime: event.deprime,
-              angoisse: event.angoisse,
-              enervement: event.enervement,
-              fatigue: event.fatigue,
+              date: event.date + "",
+              deprime: event.deprime + "",
+              angoisse: event.angoisse + "",
+              enervement: event.enervement + "",
+              fatigue: event.fatigue + "",
               comment: event.comment || "",
             },
           })
@@ -60,10 +57,9 @@ const App = () => {
     }
   }
 
-  const { control, handleSubmit, reset } = useForm();
+  const { control, handleSubmit, reset, getValues, register } = useForm();
 
   useEffect(() => {
-    console.log(dateCourante);
     async function fetchMoods() {
       try {
         let filter = {
@@ -76,6 +72,7 @@ const App = () => {
           variables: { filter: filter },
         });
         setMoods(moodData.data.listMoodss.items[0]);
+        //console.log(JSON.stringify(moodData.data.listMoodss.items[0]));
       } catch (err) {
         console.log("error fetching moods");
       }
@@ -85,15 +82,14 @@ const App = () => {
   }, [dateCourante]);
 
   useEffect(() => {
-    console.log(dateCourante);
     setDateCourante(moment().format("YYYY-MM-DD"));
   }, []);
 
   useEffect(() => {
-    console.log("useEffectRest" + dateCourante);
     reset({
       date: dateCourante,
-      deprime: moods?.deprime | "",
+      id: moods?.id,
+      deprime: moods?.deprime | "0",
       angoisse: moods?.angoisse | "0",
       fatigue: moods?.fatigue | "0",
       enervement: moods?.enervement | "0",
@@ -105,164 +101,181 @@ const App = () => {
     saveData(update);
   };
 
+  const onChangeData = () => {
+    saveData(getValues());
+  };
+
   const onChangeDateHandler = (date) => {
-    console.log(dateCourante);
     setDateCourante(moment(date).format("YYYY-MM-DD"));
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack
-          spacing={0}
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Typography
-            variant="h5"
-            sx={{ paddingTop: "20px", paddingBottom: "20px" }}
-          >
-            Humeurs
-          </Typography>
-
-          <TextField disabled sx={visuallyHidden} id="id" />
-
-          <LocalizationProvider
-            dateAdapter={DateAdapter}
-            utils={MomentUtils}
-            locale="fr"
-          >
-            <Controller
-              name="date"
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <MobileDatePicker
-                  onChange={(date) => {
-                    onChange();
-                    onChangeDateHandler(date);
-                  }}
-                  disableCloseOnSelect={false}
-                  value={value}
-                  renderInput={(params) => (
-                    <TextField sx={{ paddingBottom: "10px" }} {...params} />
-                  )}
-                />
-              )}
-            />
-          </LocalizationProvider>
-
-          <Typography sx={{ fontSize: 16 }}>Déprime :</Typography>
-
-          <Controller
-            name="deprime"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Rating
-                value={parseInt(value) || 0}
-                defaultValue={1}
-                onChange={onChange}
-                sx={{ paddingBottom: "10px" }}
-              />
-            )}
-          />
-
-          <Typography sx={{ fontSize: 16 }}>Fatigue :</Typography>
-
-          <Controller
-            name="fatigue"
-            control={control}
-            render={({ field: { onChange, value, name } }) => (
-              <Rating
-                value={parseInt(value) || 0}
-                defaultValue={1}
-                onChange={onChange}
-                sx={{ paddingBottom: "10px" }}
-              />
-            )}
-          />
-
-          <Typography sx={{ fontSize: 16 }}>Angoisse :</Typography>
-
-          <Controller
-            name="angoisse"
-            control={control}
-            render={({ field: { onChange, value, name } }) => (
-              <Rating
-                value={parseInt(value) || 0}
-                defaultValue={1}
-                onChange={onChange}
-                sx={{ paddingBottom: "10px" }}
-              />
-            )}
-          />
-
-          <Typography sx={{ fontSize: 16 }}>Enervement :</Typography>
-
-          <Controller
-            name="enervement"
-            control={control}
-            render={({ field: { onChange, value, name } }) => (
-              <Rating
-                value={parseInt(value) || 0}
-                defaultValue={1}
-                onChange={onChange}
-                sx={{ paddingBottom: "10px" }}
-              />
-            )}
-          />
-
-          <Typography variant="h6" />
-
-          <Controller
-            name="comment"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                value={value || ""}
-                sx={{ width: 260, paddingBottom: "20px" }}
-                label="Commentaire"
-                multiline
-                rows={4}
-                onChange={onChange}
-              />
-            )}
-          />
+    <>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Stack
-            spacing={2}
-            direction="row"
+            spacing={0}
+            direction="column"
             justifyContent="center"
             alignItems="center"
           >
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                setDateCourante(
-                  moment(dateCourante).add(-1, "days").format("YYYY-MM-DD")
-                );
-              }}
+            <Typography
+              variant="h5"
+              sx={{ paddingTop: "20px", paddingBottom: "20px" }}
             >
-              &lt;&lt;
-            </Button>
-            <Button variant="contained" color="primary" type="submit">
-              Enregistrer
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                setDateCourante(
-                  moment(dateCourante).add(1, "days").format("YYYY-MM-DD")
-                );
-              }}
+              Humeurs
+            </Typography>
+
+            <input type="hidden" {...register("id")} />
+
+            <LocalizationProvider
+              dateAdapter={DateAdapter}
+              utils={MomentUtils}
+              locale="fr"
             >
-              &gt;&gt;
-            </Button>
+              <Controller
+                name="date"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <MobileDatePicker
+                    onChange={(date) => {
+                      onChange();
+                      onChangeDateHandler(date);
+                    }}
+                    disableCloseOnSelect={false}
+                    value={value}
+                    renderInput={(params) => (
+                      <TextField sx={{ paddingBottom: "10px" }} {...params} />
+                    )}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+
+            <Typography sx={{ fontSize: 16 }}>Déprime :</Typography>
+
+            <Controller
+              name="deprime"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Rating
+                  value={parseInt(value) || 0}
+                  defaultValue={1}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                    onChangeData();
+                  }}
+                  sx={{ paddingBottom: "10px" }}
+                />
+              )}
+            />
+
+            <Typography sx={{ fontSize: 16 }}>Fatigue :</Typography>
+
+            <Controller
+              name="fatigue"
+              control={control}
+              render={({ field: { onChange, value, name } }) => (
+                <Rating
+                  value={parseInt(value) || 0}
+                  defaultValue={1}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                    onChangeData();
+                  }}
+                  sx={{ paddingBottom: "10px" }}
+                />
+              )}
+            />
+
+            <Typography sx={{ fontSize: 16 }}>Angoisse :</Typography>
+
+            <Controller
+              name="angoisse"
+              control={control}
+              render={({ field: { onChange, value, name } }) => (
+                <Rating
+                  value={parseInt(value) || 0}
+                  defaultValue={1}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                    onChangeData();
+                  }}
+                  sx={{ paddingBottom: "10px" }}
+                />
+              )}
+            />
+
+            <Typography sx={{ fontSize: 16 }}>Enervement :</Typography>
+
+            <Controller
+              name="enervement"
+              control={control}
+              render={({ field: { onChange, value, name } }) => (
+                <Rating
+                  value={parseInt(value) || 0}
+                  defaultValue={1}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                    onChangeData();
+                  }}
+                  sx={{ paddingBottom: "10px" }}
+                />
+              )}
+            />
+
+            <Typography variant="h6" />
+
+            <Controller
+              name="comment"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <TextField
+                  value={value || ""}
+                  onChange={(e) => {
+                    onChange(e.target.value);
+                    onChangeData();
+                  }}
+                  sx={{ width: 260, paddingBottom: "20px" }}
+                  label="Commentaire"
+                  multiline
+                  rows={4}
+                />
+              )}
+            />
+            <Stack
+              spacing={10}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setDateCourante(
+                    moment(dateCourante).add(-1, "days").format("YYYY-MM-DD")
+                  );
+                }}
+              >
+                &lt;&lt;
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setDateCourante(
+                    moment(dateCourante).add(1, "days").format("YYYY-MM-DD")
+                  );
+                }}
+              >
+                &gt;&gt;
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
